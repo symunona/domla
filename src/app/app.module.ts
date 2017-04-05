414b9d4ec8b2760acbf56001b34e41426191bbc2
puffy <nagy.gergo@3pixel.hu>
2017-04-05 12:05:21 +0200

    1.0.0-ra vissza kellett valtani (ezt a fejlesztotol tudom, mert dobtam neki mailt :)) illetve az importot maskent kellett csinalni de ez a pl-ben is ottvan
    hasznalatkor [(ngModel)]=“day.tags” a syntax ha 2 way bindingot akarsz csinalni
    
    teszteltem a cuccot ugy tunt mukodik is
    
    #1: hat talan azert mert kapalni nem akarsz :)
    #2: nem igazan van ilyesmire szukseged. Az akkor van ha sima js-es libet akarsz integralni pl jquery-t/d3-at (lsd lentebbi cli link(
    #3: Systemjs-t es a regi mitikus helyeken szerkesztett angular-cli.json mar nincs
    
    3rd party dependency install annyi, hogy npm install es utana meg esetleg typingot rantod utana. Doksija kicsit el van dugava wiki/stories alatt
    https://github.com/angular/angular-cli/wiki/stories-third-party-lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import {DayResolver} from "./shared/model/day.resolver";
 import {DayService} from "./shared/model/day.service";
 import { BlurChangeDirective } from './directives/blur-change.directive';
-import {TagInputModule} from "ng2-tag-input/dist/modules/ng2-tag-input.module";
+import {TagInputModule} from "ng2-tag-input";
 
 @NgModule({
   declarations: [
@@ -38,20 +38,8 @@ import {TagInputModule} from "ng2-tag-input/dist/modules/ng2-tag-input.module";
     HttpModule,
     AngularFireModule.initializeApp(firebaseConfig, authConfig),
     RouterModule.forRoot(routerConfig),
-    ReactiveFormsModule
-
-    // LEARN NOTE: This does not work, it does not find the resources.
-    // I am suspecting I have to configure systemJS somewhere
-    // as stated in the repo (https://www.npmjs.com/package/ng2-tag-input#configuration-for-systemjs-users)
-    // but I have no clue
-    // #1 Why do I have to do this
-    // #2 Where should I inject that data object, I tried typings.d.ts, did not work
-    // #3 Is inporting an external module such a suffering? I have seen a podcast from
-    // half year ago, and it did seem like I have to insert
-    // stuff to random places sometimes. Is this still this chaotic?
-
-    // ,TagInputModule
-
+    ReactiveFormsModule,
+    TagInputModule
   ],
   providers: [DayService, DayResolver, AuthService, AuthGuard],
   bootstrap: [AppComponent]
